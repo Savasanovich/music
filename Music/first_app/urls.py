846fb0f9beb7183ca2e5baa103841c97f90eddc9
Savasanovich <104067892+Savasanovich@users.noreply.github.com>
2022-06-13 20:52:25 +0300

    init_commit

--- Music/first_app/urls.py
+++ Music/first_app/urls.py
@@ -4,9 +4,9 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('about/', about, name='about'),
-    path('blog/', blog, name='about'),
-    path('contact/', contact, name='about'),
-    path('main/', main, name='about'),
-    path('single-blog/', single_blog, name='about'),
-    path('track/', track, name='about'),
+    path('blog/', blog, name='blog'),
+    path('contact/', contact, name='contact'),
+    path('main/', main, name='main'),
+    path('single-blog/', single_blog, name='single-blog'),
+    path('tracks/', track, name='tracks'),
 ]
